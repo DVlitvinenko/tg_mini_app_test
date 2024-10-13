b81b04de472910438618598c78c6c88abcbe2be6
DVlitvinenko <124286708+DVlitvinenko@users.noreply.github.com>
2024-10-13 19:20:34 +0300

    init

---
+++ back/index.js
@@ -0,0 +1,10 @@
+import { token } from "./secret.js";
+import TelegramBot from "node-telegram-bot-api";
+
+const bot = new TelegramBot(token, { polling: true });
+
+bot.on("message", (msg) => {
+  const chatId = msg.chat.id;
+
+  bot.sendMessage(chatId, "Received your message");
+});
---
+++ front/src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="">
+        <h1>Telegram App</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
